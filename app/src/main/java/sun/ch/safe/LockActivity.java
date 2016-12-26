caf324ea2dea3230b052a89c002b30714014e002
sunch <306535687@qq.com>
2016-12-26 12:06:31 +0800

    看门狗完成

--- app/src/main/java/sun/ch/safe/LockActivity.java
+++ app/src/main/java/sun/ch/safe/LockActivity.java
@@ -1,15 +1,64 @@
 package sun.ch.safe;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by sunch on 2016/12/22.
  */
 public class LockActivity extends Activity {
+
+    private EditText et_pwd;
+    private Button btn_sure;
+    private String aPackage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lock_activity);
+        et_pwd = (EditText) findViewById(R.id.et_pwd);
+        btn_sure = (Button) findViewById(R.id.btn_sure);
+        final Intent intent = getIntent();
+        //获取打开的应用包名
+        aPackage = intent.getStringExtra("package");
+    }
+
+    /**
+     * 点击确定
+     * @param view
+     */
+    public void sure(View view){
+        String pwd = et_pwd.getText().toString().trim();
+        if (!TextUtils.isEmpty(pwd)) {
+            if (pwd.equals("123")) {
+                //密码输入正确,发送广播到appLockService,不要拦截该应用
+                Intent intent1 = new Intent();
+                intent1.setAction("sun.ch.safe.lock");
+                intent1.putExtra("package",aPackage);//把该应用包名传递过去
+                sendBroadcast(intent1);
+                finish();
+            } else {
+                Toast.makeText(LockActivity.this, "密码不正确", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(LockActivity.this, "密码不能为空", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 监听返回键,跳到桌面
+     */
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory("android.intent.category.HOME");
+        startActivity(intent);
     }
 }
--- app/src/main/java/sun/ch/service/appLockService.java
+++ app/src/main/java/sun/ch/service/appLockService.java
@@ -2,11 +2,15 @@ package sun.ch.service;
 
 import android.app.ActivityManager;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
 import java.util.List;
+import java.util.logging.Filter;
 
 import sun.ch.dao.AppLockDao;
 import sun.ch.safe.LockActivity;
@@ -15,6 +19,10 @@ import sun.ch.safe.LockActivity;
  * Created by sunch on 2016/12/22.
  */
 public class appLockService extends Service {
+
+    private boolean flag = true;
+    private String mSkipPackage;
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -23,27 +31,49 @@ public class appLockService extends Service {
 
     @Override
     public void onCreate() {
-        while (true){
-            AppLockDao dao = new AppLockDao(this);
-            ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-            List<ActivityManager.RunningTaskInfo> runningTasks = manager.getRunningTasks(100);
-            for(ActivityManager.RunningTaskInfo task:runningTasks){
-                String packageName = task.topActivity.getPackageName();
-                boolean search = dao.search(packageName);
-                if(search){
-                    //加锁应用
-                    Intent intent = new Intent(this, LockActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(intent);
-                }else{
-                    //未加锁应用
+        //注册广播
+        MyReceiver myReceiver = new MyReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("sun.ch.safe.lock");
+        registerReceiver(myReceiver, filter);
+        new Thread() {
+            @Override
+            public void run() {
+                while (flag) {
+                    AppLockDao dao = new AppLockDao(appLockService.this);
+                    ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+                    List<ActivityManager.RunningTaskInfo> runningTasks = manager.getRunningTasks(1);
+                    ActivityManager.RunningTaskInfo task = runningTasks.get(0);
+                    String packageName = task.topActivity.getPackageName();
+                    boolean search = dao.search(packageName);
+                    if (search && !packageName.equals(mSkipPackage)) {
+                        //加锁应用
+                        Intent intent = new Intent(appLockService.this, LockActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.putExtra("package",packageName);
+                        startActivity(intent);
+                    }
                 }
+
             }
-        }
+        }.start();
+
     }
 
     @Override
     public void onDestroy() {
+        flag = false;
         super.onDestroy();
+    }
+    /**
+     * 注册广播
+     *
+     */
+    private class MyReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mSkipPackage = intent.getStringExtra("package");
+        }
     }
 }
